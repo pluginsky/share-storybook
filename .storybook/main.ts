23a968cfd24c20b1ec72d08f951ca9b8c47fca50
Jakub Biesiada <biesiadajakub@icloud.com>
2021-05-18 10:17:57 +0200

    chore: update

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,1 +1,7 @@
-import '../dist/register';
+module.exports = {
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: ["../dist/register.js"],
+};
--- src/register.tsx
+++ src/register.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import { addons, types } from '@storybook/addons';
+import { Addon, addons, types } from '@storybook/addons';
 import { AddonPanel } from '@storybook/components';
 
-import { SharePanel } from './SharePanel';
+// import { SharePanel } from './SharePanel';
 
-import { ADDON_ID, PANEL_ID, PARAM_KEY } from './shared';
+// import { ADDON_ID, PANEL_ID, PARAM_KEY } from './shared';
+import { ADDON_ID, TOOL_ID } from './constants';
+import { Tool } from './Tool';
 
 addons.register(ADDON_ID, () => {
-  const render = ({ active, key }: any) => (
-    <AddonPanel active={active} key={key}>
-      <SharePanel />
-    </AddonPanel>
-  );
+  // const render: Addon['render'] = ({ active, key }) => (
+  //   <AddonPanel active={active} key={key}>
+  //     <SharePanel />
+  //   </AddonPanel>
+  // );
 
-  const title = 'Share Storybook';
+  // const title = 'Share Storybook';
 
-  addons.add(PANEL_ID, {
-    type: types.PANEL,
-    title,
-    render,
-    paramKey: PARAM_KEY
+  addons.add(TOOL_ID, {
+    type: types.TOOL,
+    title: "My addon",
+    match: ({ viewMode }) => !!(viewMode && viewMode.match(/^(story|docs)$/)),
+    render: Tool,
   });
+
+  // addons.add(PANEL_ID, {
+  //   type: types.PANEL,
+  //   title,
+  //   render,
+  //   paramKey: PARAM_KEY
+  // });
 });
